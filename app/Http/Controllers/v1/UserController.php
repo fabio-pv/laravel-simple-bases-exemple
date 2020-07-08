d0be84bcc1e213c8df4bce79df629270baebe2e0
Fábio <fabiopv1994@gmail.com>
2020-07-08 17:21:19 -0300

    criado comando artisan para gerar o template do endpoint

--- app/Http/Controllers/v1/UserController.php
+++ app/Http/Controllers/v1/UserController.php
@@ -2,12 +2,10 @@
 
 namespace App\Http\Controllers\v1;
 
-use App\Http\Controllers\Controller;
 use App\Http\Validations\v1\UserValidation;
 use App\Models\v1\User;
 use App\Services\v1\UserService;
 use App\Transformers\v1\UserTransformer;
-use Illuminate\Http\Request;
 use LaravelSimpleBases\Http\Controllers\BaseController;
 
 class UserController extends BaseController
