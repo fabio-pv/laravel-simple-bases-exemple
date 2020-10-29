9a5e5a9712fb321c5342e50cfcb041b7db104d65
Fábio <56044466+fabio-pv@users.noreply.github.com>
2020-10-29 12:39:36 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!teste de deployer
+| contains the "web" middleware group. Now create something great!
+Super teste de deployer automatico
 |
 */
 
